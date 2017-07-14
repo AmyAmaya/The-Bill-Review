263ae0779850e164d5f091b454ca22047e4ecd71
simranm <simranminhas13@gmail.com>
2017-07-14 16:01:11 -0400

    idk

--- articles.js
+++ articles.js
@@ -1,23 +1,13 @@
-var mykeys= configs.MY_KEY;
 
-$(function(){
-    $.ajax({
-             url: "https://api.propublica.org/congress/v1/115/bills/hres" + number + ".json",
-             type: "GET",
-             dataType: 'json',
-             headers: {'X-API-Key':mykeys }
-           }).done(function(data){
-             console.log(data);
-             var bill = data.results[0];
-             var name = bill.title;
-             var summary = bill.summary;
-             var actiondate = bill.latest_major_action_date;
-             var action = bill.latest_major_action;
-             var number = bill.number;
-             document.getElementById("title").innerHTML = name;
-             document.getElementById("summary").innerHTML = summary;
-             document.getElementById("number").innerHTML = number;
-             document.getElementById("action").innerHTML = action;
-             document.getElementById("actiondate").innerHTML = actiondate;
-       })
-    })
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': '6223db961aea4ab0a89b768fb86103e3'
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
