0b9ea08e001418a5cf08d93ca4b8d1cd75a3ec42
Claire <claire@howardlabs.net>
2017-07-14 13:20:13 -0400

    changed contactrep

--- contactrep.js
+++ contactrep.js
@@ -11,15 +11,17 @@ $(function(){
     $.get('https://www.googleapis.com/civicinfo/v2/representatives?key='+mykey+'&address= '+getinfo+'',function(data){
             console.log(data)
             //$.val('#useraddress')
+            var name = data.officials[4].name
             var city = data.officials[4].address[0].city
             var line= data.officials[4].address[0].line1
             var state = data.officials[4].address[0].state
             var zip = data.officials[4].address[0].zip
-            var name = data.officials[4].name
+            var email= data.officials[4].emails[0]
             var party= data.officials[4].party
             var phone = data.officials[4].phones[0]
+            var url= data.officials[4].urls
             document.getElementById("contactinfo").innerHTML += "<div> <li> " + city + line + state
-            + zip + name + party + phone +  "</li></div>"
+            + zip + name + party + phone + email+ url + "</li></div>"
 
 
            });
