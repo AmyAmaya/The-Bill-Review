7c9ec9865583f23724b5a429f41adadacfc8524c
simranm <simranminhas13@gmail.com>
2017-07-14 11:08:37 -0400

    made bill page work

--- billpage.js
+++ billpage.js
@@ -1,20 +1,20 @@
 var mykeys= configs.MY_KEY;
 
 $(function(){
-  $.ajax({
-           url: "https://api.propublica.org/congress/v1/115/house/bills/introduced.json",
-           type: "GET",
-           dataType: 'json',
-           headers: {'X-API-Key':mykeys }
-         }).done(function(data){
-           console.log(data);
-           var bills = data.results[0].bills;
-           for(var i = 0; i < bills.length; i++){
-             var name = bills[i].title;
-             var summary = bills[i].summary;
+  var number = window.location.search.replace('?H.RES.', '').toLowerCase()
+  if(number){
+    $.ajax({
+             url: "https://api.propublica.org/congress/v1/115/bills/hres" + number + ".json",
+             type: "GET",
+             dataType: 'json',
+             headers: {'X-API-Key':mykeys }
+           }).done(function(data){
+             console.log(data);
+             var bill = data.results[0];
+             var name = bill.title;
+             var summary = bill.summary;
              document.getElementById("title").innerHTML = name;
              document.getElementById("summary").innerHTML = summary;
-
-           }
-         })
-        })
+       })
+     }
+    })
