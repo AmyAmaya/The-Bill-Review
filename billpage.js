a2d9d9e5fc884e3bc6ec0258e6d179b759972bd4
simranm <simranminhas13@gmail.com>
2017-07-14 11:28:11 -0400

    added more info on bill page html

--- billpage.js
+++ billpage.js
@@ -13,8 +13,14 @@ $(function(){
              var bill = data.results[0];
              var name = bill.title;
              var summary = bill.summary;
+             var actiondate = bill.latest_major_action_date;
+             var action = bill.latest_major_action;
+             var number = bill.number;
              document.getElementById("title").innerHTML = name;
              document.getElementById("summary").innerHTML = summary;
+             document.getElementById("number").innerHTML = number;
+             document.getElementById("action").innerHTML = action;
+             document.getElementById("actiondate").innerHTML = actiondate; 
        })
      }
     })
