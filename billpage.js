6053b54d22872d1a32002521531a726bc05e63aa
simranm <simranminhas13@gmail.com>
2017-07-19 13:49:13 -0400

    linked articles

--- billpage.js
+++ billpage.js
@@ -52,7 +52,7 @@ $(function(){
                 document.getElementById("actiondate").innerHTML = actiondate;
 
                 $("#govtrack_url").click(function(){
-                  window.open(url, '_blank')
+                  window.open('url', '_blank')
                 })
           }).done(
             $.get('https://content.guardianapis.com/search?q='+ name + '&api-key=cb3d2f14-b8dc-4e51-903b-2e5ab699dd6c',function(data){
@@ -62,7 +62,7 @@ $(function(){
                   for(var i = 0; i < 10; i++){
                     var name = articles[i].webTitle
                     var url = articles[i].webUrl
-                    document.getElementById("article").innerHTML += "<div><li>Article Name: " + name + "</li><li> Article URL: " + url + "</li></div>"
+                    document.getElementById("article").innerHTML += "<div><ul><br><hr>Article Name:" +"<a href = " +url+ ">" + name + "</a></hr></br></ul></div>"
                   }
                  })
 
